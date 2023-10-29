e2bfc1fa10d9aa003bd04deaf4f9dd87c24e8515
Gustavo Davi <gdavilima83@gmail.com>
2023-10-29 18:10:52 -0300

    :tada: intial commit

---
+++ discolver/script.js
@@ -0,0 +1,31 @@
+/*function toggleMode() {
+  
+  
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src",  "./assents/avatar-light.png")
+  } else {
+    img.setAttribute("src",   "./assents/avatar.png")
+  }
+} */
+
+function toggleMode() {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  // Obtém o tema atual da página
+  const isLightMode = html.classList.contains("light")
+
+  // Alterna entre os modos
+  if (isLightMode) {
+    // Muda para o modo noturno
+    html.classList.remove("light")
+    img.setAttribute("src", "/assents/avatar.png") // Altere o caminho conforme necessário
+  } else {
+    // Muda para o modo claro
+    html.classList.add("light")
+    img.setAttribute("src", "/assents/avatar-light.png") // Altere o caminho conforme necessário
+  }
+}
